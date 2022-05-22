06bf2e8b93587bac3e40f9c52a74f6d1165c3426
DimVinich <DmitrijVS@gmail.com>
2022-05-22 19:04:12 +0300

    Задание 1 из 5-го модуля

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -6,7 +6,20 @@ namespace Modul5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            (string Name, string[] Dishes) User;
+
+            Console.WriteLine("Ведеите имя:");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо № {0}:", i);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
